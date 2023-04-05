99831b69c4bf16773da30f379f47bfc2d74f0785
pandalee <pandaleecn@gmail.com>
2023-04-05 18:22:50 +0800

    ``

---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"encoding/json"
+	"io/ioutil"
+	"strings"
+)
+
+type Payload struct {
+	Version string `json:"version"`
+	Build   string `json:"build"`
+}
+
+func main() {
+	http.HandleFunc("/", handleRequest)
+	log.Fatal(http.ListenAndServe(":9001", nil))
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+
+	// Set CORS headers
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+    // Handle preflight requests
+    if r.Method == http.MethodOptions {
+        w.WriteHeader(http.StatusNoContent)
+        return
+    }
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("2http ListenAndServe:9001")
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+
+	var payload Payload
+	err = json.Unmarshal(body, &payload)
+	if err != nil {
+		fmt.Println("3http ListenAndServe:9001")
+		http.Error(w, "Error unmarshalling JSON", http.StatusBadRequest)
+		return
+	}
+
+	version := payload.Version
+	build := payload.Build
+
+	fmt.Println("payload.Version:", payload.Version)
+	fmt.Println("payload.Build:", payload.Build)
+
+	if version == "" || build == "" {
+		http.Error(w, "Missing version or build", http.StatusBadRequest)
+		return
+	}
+
+	ipaURL := fmt.Sprintf("http://172.16.0.94:9000/static/ipa/HelloTalk_Binary_%s_%s.ipa", version, build)
+	ipaFile := filepath.Join(os.TempDir(), fmt.Sprintf("HelloTalk_Binary_%s_%s.ipa", version, build))
+
+	// Download the IPA file
+	err = downloadFile(ipaFile, ipaURL)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error downloading IPA file: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	// Get device ID using idevice_id
+	deviceIDOutput, err := exec.Command("idevice_id", "-l").Output()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error getting device ID: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	deviceID := strings.TrimSpace(string(deviceIDOutput))
+	if deviceID == "" {
+		http.Error(w, "No device found", http.StatusInternalServerError)
+		return
+	}
+
+	// Install the app using ideviceinstaller
+	cmd := exec.Command("ideviceinstaller", "-u", deviceID, "-i", ipaFile)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Printf("Error executing ideviceinstaller: %v", err)
+		http.Error(w, fmt.Sprintf("Error installing app: %s", output), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "App installed successfully: %s", output)
+}
+
+func downloadFile(filepath string, url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
---
+++ package.md
@@ -0,0 +1,72 @@
+要将上面的 Go 服务及其依赖项打包成一个 Homebrew 安装包并支持开机启动，请按照以下步骤操作：
+
+首先，我们需要将 Go 服务编译为二进制文件。假设服务的源代码位于名为 go_ipa_installer 的目录中，可以运行以下命令进行编译：
+```sh
+cd go_ipa_installer
+go build -o go_ipa_installer main.go
+```
+
+创建一个 GitHub 仓库，将编译好的二进制文件和其他依赖项（如 ideviceinstaller 和 idevice_id）添加到该仓库。这将允许 Homebrew 从 GitHub 仓库下载这些文件。
+在仓库根目录下创建一个名为 go-ipa-installer.rb 的 Homebrew Formula 文件，其中包含以下内容：
+
+```ruby
+class GoIpaInstaller < Formula
+  desc "Go IPA Installer service for installing iOS apps over HTTP"
+  homepage "https://github.com/yourusername/go_ipa_installer" # Replace 'yourusername' with your actual GitHub username
+  url "https://github.com/yourusername/go_ipa_installer/archive/refs/tags/v1.0.0.tar.gz" # Replace with the correct URL to the tarball of the release
+  sha256 "PUT_THE_SHA256_HASH_OF_YOUR_TARBALL_HERE"
+  license "MIT"
+
+  depends_on "go" => :build
+  depends_on "libimobiledevice"
+  depends_on "ideviceinstaller"
+
+  def install
+    bin.install "go_ipa_installer"
+  end
+
+  plist_options startup: true, manual: "go_ipa_installer"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/go_ipa_installer</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+      </dict>
+      </plist>
+    EOS
+  end
+end
+```
+
+此 Formula 包含对 libimobiledevice 和 ideviceinstaller 的依赖关系，这些依赖关系将由 Homebrew 安装。plist 方法定义了一个用于开机启动的 plist 文件。
+将 Formula 文件添加到 GitHub 仓库，并为仓库创建一个新的发布（包括源代码和二进制文件）。确保在 Formula 文件中更新发布的 URL 和 SHA256 哈希。
+创建一个 Homebrew tap，然后将 go-ipa-installer.rb Formula 文件添加到 tap 中。
+使用以下命令安装您的 Go 服务：
+
+```sh
+brew tap yourusername/yourtap # Replace 'yourusername' and 'yourtap' with the appropriate values
+brew install go-ipa-installer
+经过这些步骤后，您的 Go 服务和所需的工具都将通过 Homebrew 安装在用户的系统上。同时，服务将在系统启动时自动运行。以下是在终端中安装和启动服务的命令：
+```
+
+```sh
+brew tap yourusername/yourtap # Replace 'yourusername' and 'yourtap' with the appropriate values
+brew install go-ipa-installer
+brew services start go-ipa-installer
+```
+
+现在，您的 Go 服务将在 9001 端口上监听 HTTP 请求并处理 IPA 安装。
+请注意，这里所提到的路径和命令可能需要根据您的实际项目和仓库进行调整。此外，在发布更新时，请确保更新 Formula 文件中的版本号、URL 和 SHA256 哈希，以便用户可以轻松升级到新版本。
+如果您的团队需要使用此服务，您可以将上述安装和启动命令分享给他们。他们只需在自己的计算机上运行这些命令，就可以安装和使用您的 Go 服务了。
