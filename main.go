4fda67f3dd4e9b24b9c7c82f74fb3c508ec1bb94
pandalee <pandaleecn@gmail.com>
2023-04-10 20:39:37 +0800

    111

--- main.go
+++ main.go
@@ -11,11 +11,14 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"strings"
+	"errors"
 )
 
 type Payload struct {
-	Version string `json:"version"`
-	Build   string `json:"build"`
+	Version 	string `json:"version"`
+	Build		string `json:"build"`
+	Url			string `json:"url"`
+	Platform   	int `json:"platform"`
 }
 
 type Response struct {
@@ -70,51 +73,44 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 
 	version := payload.Version
 	build := payload.Build
+	platform := payload.Platform
+	url := payload.Url
 
-	// fmt.Println("payload.Version:", payload.Version)
-	// fmt.Println("payload.Build:", payload.Build)
-
-	if version == "" || build == "" {
+	if version == "" || build == "" || url == "" {
 		http.Error(w, "Missing version or build", http.StatusBadRequest)
 		return
 	}
 
-	ipaURL := fmt.Sprintf("http://172.16.0.94:9000/static/ipa/HelloTalk_Binary_%s_%s.ipa", version, build)
-	ipaFile := filepath.Join(os.TempDir(), fmt.Sprintf("HelloTalk_Binary_%s_%s.ipa", version, build))
+	// 获取 URL 的最后一个参数
+    parts := strings.Split(url, "/")
+	ipaFile := filepath.Join(os.TempDir(), fmt.Sprintf(parts[len(parts)-1]))
 
 	// Download the IPA file
-	err = downloadFile(ipaFile, ipaURL)
+	err = downloadFile(ipaFile, url)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Error downloading IPA file: %v", err), http.StatusInternalServerError)
 		return
 	}
 
-	// Get device ID using idevice_id
-	deviceIDOutput, err := exec.Command("idevice_id", "-l").Output()
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error getting device ID: %v", err), http.StatusInternalServerError)
-		return
+	if platform == 1 {
+		err = installApk(ipaFile)
+	} else {
+		err = installIpa(ipaFile)
 	}
 
-	deviceID := strings.TrimSpace(string(deviceIDOutput))
-	if deviceID == "" {
-		http.Error(w, "No device found", http.StatusInternalServerError)
-		return
-	}
-
-	// Install the app using ideviceinstaller
-	cmd := exec.Command("ideviceinstaller", "-u", deviceID, "-i", ipaFile)
-	output, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Printf("Error executing ideviceinstaller: %v", err)
-		http.Error(w, fmt.Sprintf("Error installing app: %s", output), http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf(err.Error()), http.StatusInternalServerError)
 		return
 	}
-	
+	responseSuccessToFront(w)
+}
+
+// 安装成功，返回给前端
+func responseSuccessToFront(w http.ResponseWriter){
 	// 处理安装请求
     response := Response{
         Code:    200,
-        Message: fmt.Sprintf("App installed successfully: %s", output),
+        Message: fmt.Sprintf("App installed successfully"),
     }
     json, err := json.Marshal(response)
     if err != nil {
@@ -126,6 +122,43 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
     w.Write(json)
 }
 
+// 安装 Android 应用
+func installApk(apkFile string)(errRes error) {
+	cmd := exec.Command("adb", "install", apkFile)
+	err := cmd.Run()
+	if err != nil {
+		errRes = errors.New(fmt.Sprintf("Failed to install APK: %v", err))
+		return
+	}
+	return
+}
+
+// 安装 iOS 应用
+func installIpa(ipaFile string)(errRes error) {
+	// Get device ID using idevice_id
+	deviceIDOutput, err := exec.Command("idevice_id", "-l").Output()
+	if err != nil {
+		errRes = errors.New(fmt.Sprintf("An error occurred: %s", err))
+		return
+	}
+
+	deviceID := strings.TrimSpace(string(deviceIDOutput))
+	if deviceID == "" {
+		errRes = errors.New("No device found")
+		return
+	}
+
+	// Install the app using ideviceinstaller
+	cmd := exec.Command("ideviceinstaller", "-u", deviceID, "-i", ipaFile)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Printf("Error executing ideviceinstaller: %v", err)
+		errRes = errors.New(fmt.Sprintf("Error installing app: %s", output))
+	}
+	return
+}
+
+// 下载安装包
 func downloadFile(filepath string, url string) error {
 	resp, err := http.Get(url)
 	if err != nil {
